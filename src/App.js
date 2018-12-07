4343b61ead9347ac613231eee2037d3aac6e1779
Zak Laughton <zlaughton@gmail.com>
2018-12-06 18:05:02 -0600

    Add background animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+  },
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+         params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
