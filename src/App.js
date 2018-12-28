f14dfe8d9dc60581a678b6cee538732c6308fc01
Zak Laughton <zlaughton@gmail.com>
2018-12-27 23:37:35 -0600

    Move URLs to point to Heroku'

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://fierce-dawn-66945.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -97,7 +97,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://fierce-dawn-66945.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
